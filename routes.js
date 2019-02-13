d7067bcd0aa6db5c20572bb42e3a1335fe170ac2
ali-axghar <aliiaxghr@gmail.com>
2019-02-13 22:49:03 +0500

    update routes file

--- routes.js
+++ routes.js
@@ -27,9 +27,13 @@ method: get  Discription   authenticate the user
 ///////////////////
 for Sport table routes
 
+ list of attributes ::::: sportName , links , sportImage ,  isDeleted , deletedAt 
+ 
+////////////
+
 1 api / sports / add
 method: POST    Discription: register the user
-required::: s_name
+required::: SportName
 
 2 api / sports / get /: id
 method: GET    Discription: get  the sport
@@ -48,9 +52,11 @@ required: id
 
 for Team table routes
 
+ list of attributes ::: teamName ,captain ,teamImage ,Discription , isDeleted , deletedAt 
+
 1 api / teams / add
 method: POST    Discription: register the user
-required::: s_name
+required::: teamName  , captain   
 
 2 api / teams / get /: id
 method: GET    Discription: get  the sport
@@ -62,6 +68,31 @@ required::: id
 
 4  api / teams / delete /:id
 method: Delete   Discription: Delete a delete a sport
+required: id
+
+
+///////////////
+
+
+
+for " events " table routes
+
+ list of attributes ::: location , maker , eventName , status , duration , description , isDeleted , deletedAt 
+
+1 api / events / add
+method: POST    Discription:  add the events
+required::: eventName , maker  
+
+2 api / events / get /: id
+method: GET    Discription: get  the event
+required: id
+
+3 api / events / update /: id
+method: PUT    Discription: update the event
+required::: id
+
+4  api / events / delete /:id
+method: Delete   Discription: Delete a delete a event
 required: id
 
 
